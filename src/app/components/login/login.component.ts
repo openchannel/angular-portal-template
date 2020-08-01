452771062bb0dba6d8116d6f36ad9cd725c96745
Dhaval Gohil <dgohil@tenupsoft.com>
2020-08-01 21:50:26 +0530

    Changes according to common-components and login functionality,forgot password functionality and updates in signup functionality.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OauthService, SellerSignin } from 'oc-ng-common-service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,9 @@ export class LoginComponent implements OnInit {
 
   signupUrl = "/signup";
   forgotPwdUrl = "/forgot-password";
+  signIn = new SellerSignin();
 
-  constructor(
+  constructor(private oauthService : OauthService
   ) { }
 
   ngOnInit(): void {
@@ -18,8 +21,15 @@ export class LoginComponent implements OnInit {
 
   login(event) {
     console.log(event);
-    if (event === true) {
-      //api code 
+    if(event.submitter){
+      this.signIn.email = event.submitter.form[0].value;
+      this.signIn.password = event.submitter.form[1].value;
+      this.signIn.grant_type = "password";
+      this.signIn.clientId = environment.client_id;
+      this.signIn.clientSecret = environment.client_secret;
+      this.oauthService.signIn(this.signIn).subscribe(res => {
+          console.log(res);
+      });
     }
   }
 
--- src/app/components/users/forgot-password/forgot-password.component.ts
+++ src/app/components/users/forgot-password/forgot-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SellerService } from 'oc-ng-common-service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,16 +10,26 @@ import { Router } from '@angular/router';
 export class ForgotPasswordComponent implements OnInit {
 
   signupUrl = "/signup";
-  loginUrl = "/login"
-  constructor() { }
+  loginUrl = "/login";
+  companyLogoUrl = "./assets/img/logo-company.png";
+  forgotPasswordDoneIconPath = "./assets/img/forgot-password-complete-icon.svg";
+  forgotPwdPageState: boolean = true;
+  constructor(private sellerService: SellerService,private router: Router) { }
 
   ngOnInit(): void {
   }
 
   resetPwd(event) {
     console.log(event);
-    if (event === true) {
-      //api code 
+    if (event.submitter) {
+      var email = event.submitter.form[0].value;
+      if (email && email!="") {
+        this.sellerService.resetForgotPassword(email).subscribe(res => {
+          this.forgotPwdPageState = false; 
+      });  
+      }else {
+          this.router.navigateByUrl(this.loginUrl);
+      }
     }
   }
 
