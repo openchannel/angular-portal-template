91beeb87e4a510f0ec43a92cd2f21e03bdc7562e
Dhaval Gohil <dgohil@tenupsoft.com>
2020-08-01 10:44:18 +0530

    updated modal as per service changes.

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { UserServiceService, SignUp } from 'oc-ng-common-service';
+import {SellerSignup, SellerService } from 'oc-ng-common-service';
 
 @Component({
   selector: 'app-signup',
@@ -9,23 +9,47 @@ import { UserServiceService, SignUp } from 'oc-ng-common-service';
 export class SignupComponent implements OnInit {
 
   loginUrl = "/login";
-
-  constructor(private userService: UserServiceService) { }
+  signupModel :SellerSignup;
+  requestObj : any =  {
+    "developer":{
+      "name": ""
+  },
+  "developerAccount":{
+      "name":"",
+      "email":""
+  },
+  "extra":{
+      "password": ""
+  }
+  }; 
+  constructor(private sellerService : SellerService) {
+     this.signupModel = new SellerSignup();
+   }
 
   ngOnInit(): void {
+ 
+  }
+  
+  signup(event) {
+    console.log(event.submitter);
+    if(event.submitter){
+      this.requestObj.developerAccount.name = event.submitter.form[0].value;
+      this.requestObj.developer.name = event.submitter.form[1].value;
+      this.requestObj.developerAccount.email = event.submitter.form[2].value;
+      this.requestObj.extra.password = event.submitter.form[3].value;
+      this.sellerService.signup(this.requestObj).subscribe(res => {
+        console.log(res);
+      });
+    }
   }
 
-  signup(event) {
-    console.log(event);
-    if (event === true) {
-      let model = new SignUp();
-      this.userService.signup(model).subscribe(res => {
-        console.log(res);
-      }
-
-      );
-      console.log(this.userService.signup(model));
-    }
+  createApiRequestObj(signupModel:SellerSignup){     
+    console.log("sign up modal => "+JSON.stringify(signupModel));
+    this.requestObj.developer.name = signupModel.company;
+    this.requestObj.developerAccount.name = signupModel.uname;
+    this.requestObj.developerAccount.email = signupModel.email;
+    this.requestObj.extra.password = signupModel.password;
+    console.log("request obj => "+JSON.stringify(this.requestObj));
   }
 
 }
