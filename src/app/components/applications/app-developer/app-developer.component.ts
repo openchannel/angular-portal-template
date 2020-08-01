59c64d9b2ad0e764d0d485e5ef8e8e38140eda58
zinal_mehta <zmehta@tenupsoft.com>
2020-08-01 20:33:52 +0530

    Integrated API in manage apps component

--- src/app/components/applications/app-developer/app-developer.component.ts
+++ src/app/components/applications/app-developer/app-developer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyValuePairMapper, ChartService, AppService, Application } from 'oc-ng-common-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-developer',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppDeveloperComponent implements OnInit {
 
-  constructor() { }
+  labels = [];
+  dataSets = [];
+  count = 40;
 
-  ngOnInit(): void {
+  radioGrp: string[];
+  modelText = 'month';
+
+  chartStaticstics: KeyValuePairMapper[];
+  chartProcessing = false;
+  isProcessing = false;
+
+  appList = new Application();
+  fields = [];
+
+  selectedChartField = "downloads";
+
+  constructor(public chartService: ChartService, public appService: AppService, public router: Router) {
+
+    var downloadObj = {
+      key: "Downloads",
+      value: "downloads"
+    }
+    this.fields.push(downloadObj);
+    var viewObj = {
+      key: "Views",
+      value: "views"
+    }
+    this.fields.push(viewObj);
+
   }
 
+  ngOnInit(): void {
+    this.getChartStatistics();
+    this.getApps();
+  }
+
+  getValue(value) {
+    return value;
+  }
+
+
+  getChartStatistics() {
+
+    this.chartProcessing = true;
+    this.labels = [];
+    this.dataSets = [];
+
+    var obj = {
+      period: this.modelText,
+      field: this.selectedChartField,
+
+    }
+    this.chartService.getStats(obj).subscribe((res) => {
+
+      this.count = res.count;
+      this.chartStaticstics = res.data;
+      this.chartStaticstics.forEach(c => {
+        this.labels.push(c.key);
+        this.dataSets.push(c.value);
+
+      });
+
+      this.chartProcessing = false;
+      //console.log('labels ', this.labels);
+      //console.log('datasets ', this.dataSets);
+
+    }, (err) => {
+    });
+  }
+
+  getApps() {
+    this.isProcessing = true;
+    this.appService.getApps().subscribe(res => {
+      console.log(res);
+      this.appList.appList = res.list;
+      console.log(this.appList);
+      this.isProcessing = false;
+    })
+  }
+
+  newApp() {
+    this.router.navigateByUrl('app-new')
+  }
 }
