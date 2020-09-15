60b37d7fc5a5117c1cea5cecb01ea2959c33ec71
Alex Tkachenko <alex@softkit.company>
2020-09-15 17:58:25 +0300

    https://openchannel.atlassian.net/browse/AT-180 add simple queries

--- src/app/graphql-client/graphql-service/graphql.service.ts
+++ src/app/graphql-client/graphql-service/graphql.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
+import {ApolloQueryResult} from '@apollo/client/core';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
@@ -7,26 +9,157 @@ import {Apollo, gql} from 'apollo-angular';
 })
 export class GraphqlService {
 
-    allCountries = gql` query countries {
-        countries (filter: {}) {
-          code
+    private allAppsQuery = gql` query getApps ($developerId: String) {
+      allApps (developerId: $developerId) {
+        name
+        appCreatedDate
+        appIcon
+        appId
+        appSubmittedDate
+        childApps{
           name
-          native
         }
-      }`;
+        created
+        customAppData
+        customFields{
+          label
+        }
+        description
+        developer{
+          name
+          username
+        }
+        developerId
+        groupId
+        isLatestVersion
+        isLive
+        isPreviousAvailable
+        lastUpdated
+        lastUpdatedDate
+        pricingModels {
+          license
+        }
+        recordFound
+        safeNames
+        statistics
+        status
+        statusChangeReason
+        submittedDate
+        type {
+          label
+        }
+        version
+      }
+    }`;
 
-    constructor(private apollo: Apollo) {
+    private formQuery = gql` query getForm($formId: String!){
+      getForm(formId: $formId){
+        name
+        createdDate
+        createdDateTime
+        description
+        fields{
+          attributes
+          category
+          defaultValue
+          deleteable
+          description
+          id
+          label
+          options
+          required
+          specialType
+          subFieldDefinitions{
+            id
+          }
+          type
+          valueIncompatible
+          wizardStep
+        }
+        formId
+      }
+    }`;
 
-        this.toConsoleAllCountries();
+    private formSubmitMutation = gql` mutation submitForm($formId: String!, $submission: FormSubmissionRequestInput!) {
+        submitForm(formId: $formId, submission: $submission) {
+            app{
+              name
+            }
+            developer{
+              name
+              username
+            }
+            email
+            formName
+            formId
+            formData
+            formDataFields {
+              label
+            }
+            name
+            submittedDate
+            submittedDateTime
+            user {
+                name
+                username
+            }
+        }
+    }`;
+
+    constructor(private apollo: Apollo) {}
+
+    testCasesToConsole() {
+
+        // this.createFormSubmission('test', {
+        //     name: '',
+        //     appId: null,
+        //     userId: null,
+        //     email: '',
+        //     formData: {
+        //         name: 'vitalii samofal',
+        //         role: 'admin',
+        //         aboutme: '<p>sdfsdfljsldfklsdjf</p>',
+        //         skills: ['angular', '123', '12', '2', '21']
+        //     }
+        // })
+        //     .subscribe(({data}) => {
+        //         console.log('GraphQL (allApps) : ' + JSON.stringify(data));
+        //     }, (error) => {
+        //         console.log('GraphQL (allApps) ERROR : ' + JSON.stringify(error));
+        //     });
+
+        // this.getForm('test')
+        //     .subscribe(({data}) => {
+        //         console.log('GraphQL (getForm) : ' + JSON.stringify(data));
+        //     }, (error) => {
+        //         console.log('GraphQL (getForm) ERROR : ' + JSON.stringify(error));
+        //     });
     }
 
-    toConsoleAllCountries() {
+    getAllApps(): Observable<ApolloQueryResult<any>> {
+        return this.apollo.query({query: this.allAppsQuery});
+    }
 
-        this.apollo.query({query: this.allCountries})
-            .subscribe(({data}) => {
-                console.log('GraphQL (allCountries) : ' + JSON.stringify(data));
-            }, (error) => {
-                console.log('GraphQL (allCountries) ERROR : ' + JSON.stringify(error));
-            });
+    getDevelopersApps(developerId: string): Observable<ApolloQueryResult<any>> {
+        return this.apollo.query({
+            query: this.allAppsQuery,
+            variables: {
+                developerId,
+            }});
+    }
+
+    getForm(formId: string): Observable<ApolloQueryResult<any>> {
+        return this.apollo.query({
+            query: this.formQuery,
+            variables: {
+                formId,
+            }});
+    }
+
+    createFormSubmission(formId: string, submission: any) {
+        return this.apollo.mutate({
+            mutation: this.formSubmitMutation,
+            variables: { formId, submission },
+        });
     }
 }
