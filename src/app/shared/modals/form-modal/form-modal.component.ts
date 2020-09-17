4a0be953c47ff6f23a9f884be424e33b7a9d8772
Julia <denisenko.sk@gmail.com>
2020-09-17 12:24:06 +0300

    Added Post request for form submission

--- src/app/shared/modals/form-modal/form-modal.component.ts
+++ src/app/shared/modals/form-modal/form-modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GraphqlService } from '../../../graphql-client/graphql-service/graphql.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-form-modal',
   templateUrl: './form-modal.component.html',
   styleUrls: ['./form-modal.component.scss']
 })
-export class FormModalComponent implements OnInit {
-
+export class FormModalComponent implements OnInit, OnDestroy {
+  /**
+   * Object with all data for the form generation
+   */
   @Input() formData: any;
+
+  private subscriber: Subscription = new Subscription();
+
   constructor(private activeModal: NgbActiveModal,
               private graphQLService: GraphqlService) { }
 
@@ -17,20 +23,27 @@ export class FormModalComponent implements OnInit {
   }
 
   sendFormData(formDataForSubmission) {
-    // const dataToServer = {
-    //     name: '',
-    // };
+    const dataToServer = {
+      name: '',
+      appId: null,
+      email: '',
+      formData: formDataForSubmission
+    };
 
     if (formDataForSubmission) {
-      this.graphQLService.createFormSubmission(this.formData?.formId, formDataForSubmission).subscribe(
+      this.subscriber.add(this.graphQLService.createFormSubmission(this.formData?.formId, dataToServer).subscribe(
         result => {
-          if (result?.errors) {
+          if (result?.errors.length > 0) {
             console.log(result?.errors);
           } else {
             this.activeModal.close();
           }
         }
-      );
+      ));
     }
+  }
+
+  ngOnDestroy() {
+    this.subscriber.unsubscribe();
   }
 }
