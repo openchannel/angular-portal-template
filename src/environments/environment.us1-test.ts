ed75bce5a6ff2817fe614b78478a239a9e38e943
Vitaliy Samofal <vsamofal93@gmail.com>
2020-09-18 12:40:24 +0300

    AT-177 changed the backend URL.

--- src/environments/environment.us1-test.ts
+++ src/environments/environment.us1-test.ts
@@ -8,7 +8,7 @@ export const environment = {
     client_id: '43m9qbjigr8ed6i1het1rdnfso',
     client_secret: 'on5jmqp7uq00vgeg9tnq4pii2f1f3pm84q45uq907e8vhmr24ta',
     openchannelUrl: 'https://market.openchannel.io',
-    graphqlUrl: 'https://9134ffe0461f.ngrok.io/graphql',
+    graphqlUrl: 'https://c522a1401433.ngrok.io/graphql',
     auth: 'Basic NWY1NGE3NjljOTZjZjYxMjNlNzNiMDZmOmVJUG1fQ3F5NXVTVm1vbXdLNlNLakh1cEdQX3drN0hEY29kSS13bkZuUm8='
 };
 
