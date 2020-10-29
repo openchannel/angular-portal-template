7fa77d0c4b689c2efe8fb5325ee17bf164f2f64a
Vitaliy Samofal <vsamofal93@gmail.com>
2020-10-29 12:58:02 +0200

    AT-314 Replaced endpoints from GraphQL to proxy (get security configs; init csrf token; login user).

--- src/app/core/services/auth-service/auth.service.ts
+++ src/app/core/services/auth-service/auth.service.ts
@@ -1,4 +1,8 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {Observable} from 'rxjs';
+import {AuthConfig, LoginRequest, LoginResponse} from './model/auth-model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +12,7 @@ export class AuthService {
   public accessToken: string;
   public refreshToken: string;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.updateVariables();
   }
 
@@ -40,4 +44,15 @@ export class AuthService {
     this.refreshToken = window.sessionStorage.getItem('refreshToken');
   }
 
+  public getAuthConfig(): Observable<AuthConfig> {
+    return this.http.get(`${environment.apiUrl}/v2/auth/config`, { withCredentials: true });
+  }
+
+  public initCsrf(): Observable<any> {
+    return this.http.get( `${environment.apiUrl}/init/csrf`, {withCredentials: true});
+  }
+
+  public loginUser(loginRequest: LoginRequest): Observable<LoginResponse> {
+    return this.http.post(`${environment.apiUrl}/v2/auth/login`, loginRequest, {withCredentials: true});
+  }
 }
---
+++ src/app/core/services/auth-service/model/auth-model.ts
@@ -0,0 +1,32 @@
+
+export interface ClaimsMappings {
+    emailField?: string;
+    firstNameField?: string;
+    lastNameField?: string;
+    organizationId?: string;
+    companyName?: string;
+}
+
+export interface AuthConfig {
+    type?: string;
+    clientId?: string;
+    issuer?: string;
+    grantType?: string;
+    scope?: string;
+    redirectUri?: string;
+    strictDiscoveryDocumentValidation?: any;
+    claimsMappings?: ClaimsMappings;
+}
+
+export interface LoginRequest {
+    idToken?: string;
+    accessToken?: string;
+}
+
+export class LoginResponse {
+    accessToken?: string;
+    refreshToken?: string;
+}
+
+
+
