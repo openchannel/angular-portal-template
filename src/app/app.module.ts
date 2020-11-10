27f0c6db3fbdb8cd65dd7f1a889c950611e824b1
Vitaliy Samofal <vsamofal93@gmail.com>
2020-11-10 17:03:11 +0200

    AT-02 Changed http extractor/interceptor for the CSRF token. Used auth functions from the angular-common-service-library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbDateAdapter, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/template/header/header.component';
-import { FooterComponent } from './shared/template/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { HttpConfigInterceptor } from './core/interceptors/httpconfig.interceptor';
-import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
-import { LoginComponent } from './components/login/login.component';
-import { ForgotPasswordComponent } from './components/users/forgot-password/forgot-password.component';
-import { CustomComponentsModule } from './shared/custom-components/custom-components.module';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { CustomAdapter } from './core/datepicker-adapter';
-import { LoaderComponent } from './shared/custom-components/loader/loader.component';
-import { DatePipe } from '@angular/common';
-import { SignupComponent } from './components/signup/signup.component';
-import { OcCommonServiceModule } from 'oc-ng-common-service';
-import { AppStoreComponent } from './components/applications/app-store/app-store.component';
-import { AppAppsComponent } from './components/applications/app-apps/app-apps.component';
-import { AppDetailComponent } from './components/applications/app-detail/app-detail.component';
-import { AppDeveloperComponent } from './components/applications/app-developer/app-developer.component';
-import { AppNewComponent } from './components/applications/app-new/app-new.component';
-import { environment } from 'src/environments/environment';
-import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/template/header/header.component';
+import {FooterComponent} from './shared/template/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {HttpConfigInterceptor} from './core/interceptors/httpconfig.interceptor';
+import {CommonLayoutComponent} from './layouts/common-layout/common-layout.component';
+import {LoginComponent} from './components/login/login.component';
+import {ForgotPasswordComponent} from './components/users/forgot-password/forgot-password.component';
+import {CustomComponentsModule} from './shared/custom-components/custom-components.module';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {CustomAdapter} from './core/datepicker-adapter';
+import {LoaderComponent} from './shared/custom-components/loader/loader.component';
+import {DatePipe} from '@angular/common';
+import {SignupComponent} from './components/signup/signup.component';
+import {OcCommonServiceModule} from 'oc-ng-common-service';
+import {AppStoreComponent} from './components/applications/app-store/app-store.component';
+import {AppAppsComponent} from './components/applications/app-apps/app-apps.component';
+import {AppDetailComponent} from './components/applications/app-detail/app-detail.component';
+import {AppDeveloperComponent} from './components/applications/app-developer/app-developer.component';
+import {AppNewComponent} from './components/applications/app-new/app-new.component';
+import {environment} from 'src/environments/environment';
+import {FroalaEditorModule, FroalaViewModule} from 'angular-froala-wysiwyg';
 import 'froala-editor/js/plugins.pkgd.min.js';
 import 'froala-editor/js//plugins/code_view.min.js';
-import { GeneralProfileComponent } from './components/my-profile/general/general-profile.component';
-import { ChangePasswordComponent } from './components/my-profile/change-password/change-password.component';
-import { MyProfileComponent } from './components/my-profile/my-profile.component';
-import { EditAppDetailComponent } from './components/applications/edit-app-detail/edit-app-detail.component';
-import { EditAppComponent } from './components/applications/edit-app/edit-app.component';
-import { ActivationComponent } from './components/activation/activation.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { FormListGeneratorComponent } from './components/applications/app-store/form-list-generator/form-list-generator.component';
-import { FormModalComponent } from './shared/modals/form-modal/form-modal.component';
+import {GeneralProfileComponent} from './components/my-profile/general/general-profile.component';
+import {ChangePasswordComponent} from './components/my-profile/change-password/change-password.component';
+import {MyProfileComponent} from './components/my-profile/my-profile.component';
+import {EditAppDetailComponent} from './components/applications/edit-app-detail/edit-app-detail.component';
+import {EditAppComponent} from './components/applications/edit-app/edit-app.component';
+import {ActivationComponent} from './components/activation/activation.component';
+import {ResetPasswordComponent} from './components/reset-password/reset-password.component';
+import {FormListGeneratorComponent} from './components/applications/app-store/form-list-generator/form-list-generator.component';
+import {FormModalComponent} from './shared/modals/form-modal/form-modal.component';
 import {APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLink} from 'apollo-angular/http';
 import {ApolloClientOptions, DefaultOptions, InMemoryCache} from '@apollo/client/core';
 import {OAuthModule} from 'angular-oauth2-oidc';
-import {AppService} from "./core/api/app.service";
+import {AppService} from './core/api/app.service';
 import {AppListComponent} from './components/applications/app-apps/app-list/app-list.component';
 import {CreateAppComponent} from './components/applications/app-apps/app-create-app/create-app.component';
-import {OcCommonLibModule, OcDropboxComponent} from 'oc-ng-common-component';
+import {OcCommonLibModule} from 'oc-ng-common-component';
 import {AppsServiceImpl} from './core/services/apps-services/model/apps-service-impl';
 import {MockAppsService} from './core/services/apps-services/mock-apps-service/mock-apps-service.service';
-import { ConfirmationModalComponent } from './shared/modals/confirmation-modal/confirmation-modal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { CamelCaseToNormalPipe } from './shared/custom-components/camel-case-to-normal.pipe';
-import { SubmissionsTableComponent } from './components/applications/app-store/form-list-generator/submissions-table/submissions-table.component';
-import { SubmissionsDataViewModalComponent } from './shared/modals/submissions-data-view-modal/submissions-data-view-modal.component';
-import {AuthService} from "./core/services/auth-service/auth.service";
-import {setContext} from "@apollo/client/link/context";
-import {HttpXsrfInterceptor} from "./core/interceptors/httpxsft.interceptor";
+import {ConfirmationModalComponent} from './shared/modals/confirmation-modal/confirmation-modal.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CamelCaseToNormalPipe} from './shared/custom-components/camel-case-to-normal.pipe';
+import {SubmissionsTableComponent} from './components/applications/app-store/form-list-generator/submissions-table/submissions-table.component';
+import {SubmissionsDataViewModalComponent} from './shared/modals/submissions-data-view-modal/submissions-data-view-modal.component';
+import {AuthService} from './core/services/auth-service/auth.service';
+import {setContext} from '@apollo/client/link/context';
+import {HttpXsrfInterceptor} from './core/interceptors/httpxsft.interceptor';
+import {HttpXsrfExtractor} from './core/interceptors/httpxsft.extractor';
 
 export function createApollo(httpLink: HttpLink, authService: AuthService): ApolloClientOptions<any> {
 
@@ -148,6 +149,7 @@ export function createApollo(httpLink: HttpLink, authService: AuthService): Apol
   providers: [
    { provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true },
    { provide: HTTP_INTERCEPTORS, useClass: HttpXsrfInterceptor, multi: true },
+   { provide: HTTP_INTERCEPTORS, useClass: HttpXsrfExtractor, multi: true },
    { provide: NgbDateAdapter, useClass: CustomAdapter },
    {
      provide: APOLLO_OPTIONS,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {LoaderService} from 'src/app/shared/services/loader.service';
+import {Router} from '@angular/router';
 import {OAuthService} from 'angular-oauth2-oidc';
 import {JwksValidationHandler} from 'angular-oauth2-oidc-jwks';
 import {AppService} from '../../core/api/app.service';
 import {AuthService} from '../../core/services/auth-service/auth.service';
 import {Subscription} from 'rxjs';
-import {AuthConfig, LoginRequest} from '../../core/services/auth-service/model/auth-model';
+import {AuthConfig} from '../../core/services/auth-service/model/auth-model';
+import {AuthenticationService} from 'oc-ng-common-service';
 
 @Component({
   selector: 'app-login',
@@ -30,40 +30,38 @@ export class LoginComponent implements OnInit {
   private formSubscription: Subscription = new Subscription();
 
   constructor(private oauthService: OAuthService, private appService: AppService, private router: Router,
-              private authService: AuthService, private loaderService: LoaderService,
-              private route: ActivatedRoute) {
+              private authService: AuthService,
+              private authApiService: AuthenticationService) {
   }
 
   ngOnInit(): void {
     this.isLoading = true;
     this.oauthService.hasValidAccessToken();
-    this.formSubscription.add(this.authService.getAuthConfig().subscribe(authConfig => {
+    this.formSubscription.add(this.authApiService.getAuthConfig().subscribe(authConfig => {
 
-        this.authConfig = authConfig;
-        this.oauthService.configure({
-          ...authConfig,
-          redirectUri: authConfig.redirectUri || window.location.origin
-        });
+          this.authConfig = authConfig;
+          this.oauthService.configure({
+            ...authConfig,
+            redirectUri: authConfig.redirectUri || window.location.origin
+          });
 
-        this.oauthService.tokenValidationHandler = new JwksValidationHandler();
-        this.oauthService.loadDiscoveryDocumentAndTryLogin({
-          onTokenReceived: receivedTokens => {
+          this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+          this.oauthService.loadDiscoveryDocumentAndTryLogin({
+            onTokenReceived: receivedTokens => {
+              this.authApiService.login({
+                idToken: receivedTokens.idToken,
+                accessToken: receivedTokens.accessToken
+              }).subscribe(loginResponse => {
+                this.authService.persist(loginResponse.accessToken, loginResponse.refreshToken);
+                this.authService.testSetAuthJwtToken(loginResponse.accessToken);
+                this.router.navigate(['/app-store']);
+              });
 
-            const loginRequest: LoginRequest = {
-              idToken: receivedTokens.idToken
-            };
-            this.authService.loginUser(loginRequest).subscribe(loginResponse => {
-              this.authService.persist(loginResponse.accessToken, loginResponse.refreshToken);
-              this.authService.testSetAuthJwtToken(loginResponse.accessToken);
-              this.router.navigate(['/app-store']);
-            });
-
-            this.tokenInfo = JSON.stringify(receivedTokens, null, 4);
-          }
-        });
-
-      }, err => console.error('getAuthConfig', err),
-      () => this.isLoading = false));
+              this.tokenInfo = JSON.stringify(receivedTokens, null, 4);
+            }
+          });
+        }, err => console.error('getAuthConfig', err),
+        () => this.isLoading = false));
   }
 
   login() {
