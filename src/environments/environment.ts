0ddbb9fcf3bdf14aabbd5c2a45ef28f49c2b3eca
Vitaliy Samofal <vsamofal93@gmail.com>
2020-11-23 11:27:40 +0200

    AT-468 Removed commented code.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,6 @@ export const environment = {
   client_secret: 'on5jmqp7uq00vgeg9tnq4pii2f1f3pm84q45uq907e8vhmr24ta',
   openchannelUrl: 'https://market.openchannel.io',
   graphqlUrl: 'http://6e4d6c7f6f0f.ngrok.io/graphql',
-  // graphqlUrl: 'http://60da39e541f9.ngrok.io/graphql',
   auth: 'Basic NWY1NGE3NjljOTZjZjYxMjNlNzNiMDZmOmVJUG1fQ3F5NXVTVm1vbXdLNlNLakh1cEdQX3drN0hEY29kSS13bkZuUm8='
 };
 
