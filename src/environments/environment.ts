2a8f7e42a439a4df85a0afaa5077a7614ff2e2c9
Alex Tkachenko <alex@softkit.company>
2020-11-25 13:30:26 +0200

    removed unused properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8091/',
-  client_id: '43m9qbjigr8ed6i1het1rdnfso',
-  client_secret: 'on5jmqp7uq00vgeg9tnq4pii2f1f3pm84q45uq907e8vhmr24ta',
-  openchannelUrl: 'https://market.openchannel.io',
-  graphqlUrl: 'http://2547ac121424.ngrok.io/graphql',
-  auth: 'Basic NWY1NGE3NjljOTZjZjYxMjNlNzNiMDZmOmVJUG1fQ3F5NXVTVm1vbXdLNlNLakh1cEdQX3drN0hEY29kSS13bkZuUm8='
 };
 
 /*
