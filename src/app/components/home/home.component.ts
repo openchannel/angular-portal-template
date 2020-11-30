24b8e15d21ffc4833f6f41384e58c09643951960
Julia <denisenko.sk@gmail.com>
2020-11-30 11:30:58 +0200

    Updated redirect links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,4 +17,8 @@ export class HomeComponent implements OnInit {
       this.router.navigate(['app-developer']).then();
     }
   }
+
+  getStartedRedirect() {
+    this.router.navigate(['signup']).then();
+  }
 }
