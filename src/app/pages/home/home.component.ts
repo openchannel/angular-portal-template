f00f178e6a415dea0d5cd58bc6bdddd16b222d9a
Julia <denisenko.sk@gmail.com>
2021-02-05 21:07:01 +0200

    TE-718: Created special title for the Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthHolderService } from 'oc-ng-common-service';
+import { AuthHolderService, TitleService } from 'oc-ng-common-service';
 import { Router } from '@angular/router';
+import { siteConfig } from '../../../assets/data/siteConfig';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,11 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   constructor(private authHolderService: AuthHolderService,
-              private router: Router) { }
+              private router: Router,
+              private titleService: TitleService) { }
 
   ngOnInit() {
+    this.titleService.setPostfix(siteConfig.tagline);
     if (this.authHolderService.isLoggedInUser()) {
       this.router.navigate(['/app/manage']).then();
     }
