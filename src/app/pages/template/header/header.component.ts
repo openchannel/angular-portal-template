e7d9ca71d2113b787ab21d6208ea9991c7c2abf2
Julia <denisenko.sk@gmail.com>
2021-04-21 19:20:00 +0300

    AT-1198: Updated login buttons to links

--- src/app/pages/template/header/header.component.ts
+++ src/app/pages/template/header/header.component.ts
@@ -46,10 +46,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  login() {
-    this.router.navigate(['/login']);
-  }
-
   logout() {
     this.logOutService.logOut();
   }
