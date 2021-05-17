0741c3960a18433d8abd0bd893e2389d45909a40
Alex Tkachenko <alex@softkit.company>
2021-05-17 12:45:15 +0300

    AT-1196 fixed display of social links fixed import path

--- src/app/pages/template/footer/footer.component.ts
+++ src/app/pages/template/footer/footer.component.ts
@@ -1,44 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import {SocialLink} from 'oc-ng-common-component/src/lib/common-components/interfaces/social-link.model';
+import { Component } from '@angular/core';
+import { SocialLink } from '@openchannel/angular-common-components/src/lib/common-components/interfaces/social-link.model';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+    socialLinks: SocialLink[] = [
+        {
+            link: 'https://facebook.com',
+            iconSrc: 'assets/img/facebook-icon.svg',
+            iconAlt: 'facebook-icon',
+        },
+        {
+            link: 'https://instagram.com',
+            iconSrc: 'assets/img/instagram.svg',
+            iconAlt: 'instagram-icon',
+        },
+        {
+            link: 'https://linkedin.com',
+            iconSrc: 'assets/img/linkedin.svg',
+            iconAlt: 'linkedin-icon',
+        },
+        {
+            link: 'https://twitter.com',
+            iconSrc: 'assets/img/twitter-icon.svg',
+            iconAlt: 'twitter-icon',
+        },
+        {
+            link: 'https://tiktok.com',
+            iconSrc: 'assets/img/tik-tok.svg',
+            iconAlt: 'tiktok-icon',
+        },
+    ];
 
-  socialLinks: SocialLink[] = [
-    {
-      link: 'https://facebook.com',
-      iconSrc: 'assets/img/facebook-icon.svg',
-      iconAlt: 'facebook-icon'
-    },
-    {
-      link: 'https://instagram.com',
-      iconSrc: 'assets/img/instagram.svg',
-      iconAlt: 'instagram-icon'
-    },
-    {
-      link: 'https://linkedin.com',
-      iconSrc: 'assets/img/linkedin.svg',
-      iconAlt: 'linkedin-icon'
-    },
-    {
-      link: 'https://twitter.com',
-      iconSrc: 'assets/img/twitter-icon.svg',
-      iconAlt: 'twitter-icon'
-    },
-    {
-      link: 'https://tiktok.com',
-      iconSrc: 'assets/img/tik-tok.svg',
-      iconAlt: 'tiktok-icon'
-    }
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor() {}
 }
