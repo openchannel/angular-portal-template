f8803e043afd38f43245a33695aee07919360b97
Виталий Самофал <samofal.vitaliy@gmail.com>
2021-07-09 16:05:16 +0000

    Merged in feature/AT-1194_proxy_requests (pull request #328)
    AT-1194 Proxy requests
    
    * AT-1194 Added custom Netlify plugin for updating environment.ts variables.
    
    * Merge remote-tracking branch 'origin/release/1.17.0' into feature/AT-1194_proxy_requests
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/app/core/interceptors/httpconfig.interceptor.ts
    
    * Merge remote-tracking branch 'origin/release/1.17.0' into feature/AT-1194_proxy_requests
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    
    Approved-by: LarionovPavel
    Approved-by: Julia
    Approved-by: Alex

--- src/app/core/interceptors/httpconfig.interceptor.ts
+++ src/app/core/interceptors/httpconfig.interceptor.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-import { OcErrorService } from '@openchannel/angular-common-components';
-import { AuthHolderService } from '@openchannel/angular-common-services';
-import { environment } from '@env';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+import {OcErrorService} from '@openchannel/angular-common-components';
+import {AuthHolderService} from '@openchannel/angular-common-services';
+import { OC_API_URL } from 'app/app.module';
 
 @Injectable()
 export class HttpConfigInterceptor implements HttpInterceptor {
@@ -27,9 +27,9 @@ export class HttpConfigInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        if (this.authHolderService.accessToken && request.url?.startsWith(this.proxyApiUrl)) {
-            request = HttpConfigInterceptor.addToken(request, this.authHolderService.accessToken);
-        }
+    if (this.authHolderService.accessToken && request.url.startsWith(OC_API_URL)) {
+      request = HttpConfigInterceptor.addToken(request, this.authHolderService.accessToken);
+    }
 
         return next.handle(request);
     }
