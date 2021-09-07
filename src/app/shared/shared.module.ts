7ec6d84409f02425f2579631194fa8fe06204773
Alex <atkachenko@softkitit.com>
2021-09-07 12:44:06 +0000

    Merged in bugfix/AT-1495_fix_vulnerabilities (pull request #387)
    AT-1495 fixed dependency vulnerabilities
    
    * AT-1495 fixed dependency vulnerabilities
    
    * AT-1495 fixed dependency vulnerabilities
    
    
    Approved-by: Виталий Самофал

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,36 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {OcCommonLibModule} from '@openchannel/angular-common-components';
-import {CamelCaseToNormalPipe} from './pipes/camel-case-to-normal.pipe';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AppConfirmationModalComponent} from './modals/app-confirmation-modal/app-confirmation-modal.component';
-import {PermissionDirective} from './directive/permission.directive';
-import {PageTitleComponent} from './components/page-title/page-title.component';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OcCommonLibModule } from '@openchannel/angular-common-components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppConfirmationModalComponent } from './modals/app-confirmation-modal/app-confirmation-modal.component';
+import { PermissionDirective } from './directive/permission.directive';
+import { PageTitleComponent } from './components/page-title/page-title.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppConfirmationModalComponent,
-    CamelCaseToNormalPipe,
-    PermissionDirective,
-    PageTitleComponent,
-  ],
-  imports: [
-    CommonModule,
-    OcCommonLibModule,
-    ReactiveFormsModule,
-    FormsModule,
-    NgbModule,
-    RouterModule,
-  ],
-  exports: [
-    OcCommonLibModule,
-    CamelCaseToNormalPipe,
-    FormsModule,
-    ReactiveFormsModule,
-    PermissionDirective,
-    PageTitleComponent
-  ]
+    declarations: [AppConfirmationModalComponent, PermissionDirective, PageTitleComponent],
+    imports: [CommonModule, OcCommonLibModule, ReactiveFormsModule, FormsModule, NgbModule, RouterModule],
+    exports: [OcCommonLibModule, FormsModule, ReactiveFormsModule, PermissionDirective, PageTitleComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
