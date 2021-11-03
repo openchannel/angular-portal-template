d0ef6847d4311261f266983246e70e0f49ac01f1
Julia <yui@softkitit.com>
2021-11-03 18:41:56 +0200

    AT-1586: updated the general section

--- src/app/pages/general/general.module.ts
+++ src/app/pages/general/general.module.ts
@@ -1,34 +1,27 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {GeneralRoutingModule} from './general-routing.module';
-import {SignupComponent} from './signup/signup.component';
-import {LoginComponent} from './login/login.component';
-import {ResendActivationComponent} from './resend-activation/resend-activation.component';
-import {ResetPasswordComponent} from './reset-password/reset-password.component';
-import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
-import {ActivationComponent} from './activation/activation.component';
-import {SharedModule} from '@shared/shared.module';
-import {InvitedSignupComponent} from './invited-signup/invited-signup.component';
-import {OcAuthComponentsModule, OcFormComponentsModule} from '@openchannel/angular-common-components';
-
+import { GeneralRoutingModule } from './general-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ResendActivationComponent } from './resend-activation/resend-activation.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ActivationComponent } from './activation/activation.component';
+import { SharedModule } from '@shared/shared.module';
+import { InvitedSignupComponent } from './invited-signup/invited-signup.component';
+import { OcAuthComponentsModule, OcFormComponentsModule } from '@openchannel/angular-common-components';
 
 @NgModule({
-  declarations: [
-    SignupComponent,
-    LoginComponent,
-    ActivationComponent,
-    ResendActivationComponent,
-    ResetPasswordComponent,
-    ForgotPasswordComponent,
-    InvitedSignupComponent
-  ],
-  imports: [
-    CommonModule,
-    GeneralRoutingModule,
-    SharedModule,
-    OcAuthComponentsModule,
-    OcFormComponentsModule
-  ]
+    declarations: [
+        SignupComponent,
+        LoginComponent,
+        ActivationComponent,
+        ResendActivationComponent,
+        ResetPasswordComponent,
+        ForgotPasswordComponent,
+        InvitedSignupComponent,
+    ],
+    imports: [CommonModule, GeneralRoutingModule, SharedModule, OcAuthComponentsModule, OcFormComponentsModule],
 })
-export class GeneralModule { }
+export class GeneralModule {}
