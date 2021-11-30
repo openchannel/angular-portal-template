96df874647d856be33fe2b000453c21163f5838e
Alex Tkachenko <atkachenko@softkitit.com>
2021-11-30 12:47:16 +0300

    AT-1598: make path to payouts optional

--- src/app/pages/account-management/account-management-routing.module.ts
+++ src/app/pages/account-management/account-management-routing.module.ts
@@ -4,6 +4,13 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyCompanyComponent } from './my-company/my-company.component';
 import { AuthGuard } from '@core/guards/auth.guard';
 import { NativeLoginGuard } from '@core/guards/native-login.guard';
+import { siteConfig } from '../../../assets/data/siteConfig';
+import { PaymentsGateways } from '@openchannel/angular-common-services';
+
+const paymentsEnabledSubPaths =
+    siteConfig.paymentsEnabled && siteConfig.paymentsGateway === PaymentsGateways.STRIPE
+        ? [{ path: 'payouts', component: MyCompanyComponent, canActivate: [AuthGuard] }]
+        : [];
 
 const routes: Routes = [
     {
@@ -20,7 +27,7 @@ const routes: Routes = [
         children: [
             { path: 'company-details', component: MyCompanyComponent, canActivate: [AuthGuard] },
             { path: 'user-management', component: MyCompanyComponent, canActivate: [AuthGuard] },
-            { path: 'payouts', component: MyCompanyComponent, canActivate: [AuthGuard] },
+            ...paymentsEnabledSubPaths,
         ],
     },
 ];
