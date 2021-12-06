ec7d56acb6c0953f01364c83f60e7eaa9757acc7
Alex Tkachenko <atkachenko@softkitit.com>
2021-12-06 13:25:41 +0300

    AT-1555: add stripe accounts service

---
+++ src/app/core/services/stripe-accounts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { StripeService } from '@openchannel/angular-common-services';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class StripeAccountsService {
+    constructor(private stripeService: StripeService) {}
+
+    getIsAccountConnected(): Observable<boolean> {
+        return this.stripeService.getConnectedAccounts().pipe(map(res => !!res.accounts.length));
+    }
+}
