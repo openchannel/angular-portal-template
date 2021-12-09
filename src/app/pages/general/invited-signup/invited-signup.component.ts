e399a26804b9cb3bfc04cb09a8a4d19ebcb10e6c
Pavel <pavellarionov@softkit.company>
2021-12-09 16:49:14 +0200

    bugfix AT-1303 added toastr message when invite is deleted while signup

--- src/app/pages/general/invited-signup/invited-signup.component.ts
+++ src/app/pages/general/invited-signup/invited-signup.component.ts
@@ -14,6 +14,7 @@ import { LoadingBarService } from '@ngx-loading-bar/core';
 import { merge } from 'lodash';
 import { LogOutService } from '@core/services/logout-service/log-out.service';
 import { AppFormField } from '@openchannel/angular-common-components';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-invited-signup',
@@ -49,6 +50,7 @@ export class InvitedSignupComponent implements OnInit, OnDestroy {
         private nativeLoginService: NativeLoginService,
         private logOutService: LogOutService,
         private loadingBar: LoadingBarService,
+        private toaster: ToastrService,
     ) {}
 
     ngOnInit(): void {
@@ -120,6 +122,7 @@ export class InvitedSignupComponent implements OnInit, OnDestroy {
                         }
                     },
                     () => {
+                        this.toaster.error('Invite has been deleted');
                         this.router.navigate(['']).then();
                     },
                 );
