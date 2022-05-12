388affe6455ceba7c185e546f51a48318aa7a1c3
Artem Koteluk <akoteliuk@softkitit.com>
2022-05-13 02:10:04 +0300

    AT-1796-Move-unit-tests-from-market-to-portal: created tests form not-found-component

--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -1,21 +1,26 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
+import { PrerenderRequestsWatcherService } from '@openchannel/angular-common-services';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { mockPrerenderRequestsWatcherService } from '../../../mock/providers.mock';
-import { MockButtonComponent } from '../../../mock/components.mock';
+import { Router } from '@angular/router';
+import { MockButtonComponent, MockRoutingComponent } from '../../../mock/components.mock';
+import { MockPrerenderRequestsWatcherService } from '../../../mock/services.mock';
 
 describe('NotFoundComponent', () => {
     let component: NotFoundComponent;
     let fixture: ComponentFixture<NotFoundComponent>;
+    let router: Router;
 
     beforeEach(
         waitForAsync(() => {
             TestBed.configureTestingModule({
-                declarations: [NotFoundComponent, MockButtonComponent],
-                providers: [mockPrerenderRequestsWatcherService()],
-                imports: [RouterTestingModule],
+                declarations: [NotFoundComponent, MockButtonComponent, MockRoutingComponent],
+                imports: [RouterTestingModule.withRoutes([{ path: 'some-page', component: MockRoutingComponent }])],
+                providers: [{ useClass: MockPrerenderRequestsWatcherService, provide: PrerenderRequestsWatcherService }],
             }).compileComponents();
+            router = TestBed.inject(Router);
         }),
     );
 
@@ -27,5 +32,25 @@ describe('NotFoundComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should route to / when home page link clicked', fakeAsync(() => {
+        router.navigate(['some-page']);
+        tick();
+
+        const homeLink = fixture.debugElement.query(By.css('.not-found__home-page-button')).nativeElement;
+        homeLink.click();
+        tick();
+
+        expect(router.url).toBe('/');
+    }));
+
+    it('should set scroll to 0 0 position when going to home page', () => {
+        window.scrollTo = jest.fn();
+
+        const homeButton = fixture.debugElement.query(By.css('.not-found__home-page-button oc-button')).nativeElement;
+        homeButton.click();
+
+        expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
     });
 });
